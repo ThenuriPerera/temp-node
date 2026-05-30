71983032e3cc797d759a625b0b5ad6caa9eaf09c
Admin <thenunp@gmail.com>
2026-05-30 16:04:57 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,19 @@
+const names = require('./4-names');
+console.log(names);
+
+//sayHi('susan');
+//sayHi(john);
+//sayHi(peter);
+
+
+const sayHis = require('./5-utils');
+
+
+//sayHis('susan');
+//sayHis(names.john);
+//sayHis(names.peter);
+
+const data = require('./6-alternative-flavor');
+console.log(data);
+
+require('./7-mind-grenade');
